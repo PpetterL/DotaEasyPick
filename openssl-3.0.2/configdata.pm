55ac66d7d38c2e0ef12a4c71a8399506c788c6f5
PpetterL <103736203+PpetterL@users.noreply.github.com>
2022-05-07 10:39:57 +0200

    Revert "Autoupdate match_seq_num.txt and Wins.json"
    This reverts commit 0cf5ea75c4df825e1b2d6c6ebfae3f35237ad247.

--- openssl-3.0.2/configdata.pm
+++ openssl-3.0.2/configdata.pm
@@ -8091,6 +8091,9 @@ our %unified_info = (
         },
         "apps/lib" => {
             "deps" => [
+                "apps/lib/openssl-bin-cmp_mock_srv.o",
+                "apps/lib/cmp_client_test-bin-cmp_mock_srv.o",
+                "apps/lib/uitest-bin-apps_ui.o",
                 "apps/lib/libapps-lib-app_libctx.o",
                 "apps/lib/libapps-lib-app_params.o",
                 "apps/lib/libapps-lib-app_provider.o",
@@ -8110,10 +8113,7 @@ our %unified_info = (
                 "apps/lib/libapps-lib-tlssrp_depr.o",
                 "apps/lib/libapps-lib-win32_init.o",
                 "apps/lib/libtestutil-lib-opt.o",
-                "apps/lib/libtestutil-lib-win32_init.o",
-                "apps/lib/openssl-bin-cmp_mock_srv.o",
-                "apps/lib/cmp_client_test-bin-cmp_mock_srv.o",
-                "apps/lib/uitest-bin-apps_ui.o"
+                "apps/lib/libtestutil-lib-win32_init.o"
             ],
             "products" => {
                 "bin" => [
